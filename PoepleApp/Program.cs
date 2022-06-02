844ea44151b2312f4ccbe709dbe140d6788cf981
AyaneSHOJI <shojiayane@gmail.com>
2022-06-02 12:38:30 +0200

    p.186 setting and outputting field values

--- PoepleApp/Program.cs
+++ PoepleApp/Program.cs
@@ -1,5 +1,23 @@
 ﻿using Packt.Shared;
 using static System.Console;
+using System;
 
 Person bob = new();
-WriteLine(bob.ToString()); 
+WriteLine(bob.ToString()); //toString method in the System.Object shows the full namespace and type name
+
+bob.Name = "Bob Smith";
+bob.DateOfBirth = new DateTime(1965, 12, 22);
+
+WriteLine(format: "{0} was born on {1:dddd, d MMMM yyyy}",
+            arg0: bob.Name,
+            arg1: bob.DateOfBirth);
+
+Person alice = new()
+{
+    Name = "Alice Jones",
+    DateOfBirth = new DateTime(1998, 3, 7)
+};
+
+WriteLine(format: "{0} was born on {1:dddd, d MMMM yyyy}",
+            arg0: alice.Name,
+            arg1: alice.DateOfBirth);
--- cs10net6chapter05/Person.cs
+++ cs10net6chapter05/Person.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Text;
 using static System.Console;
 
-namespace Packt.Shared
+namespace Packt.Shared 
 {
-    public class Person
+    public class Person : Object //F12 to see definition
     {
+        // fields
+        public string Name;
+        public DateTime DateOfBirth;
 
     }
 }
