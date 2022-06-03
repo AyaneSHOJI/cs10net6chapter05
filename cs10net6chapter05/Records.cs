dc592fe79d9de0beaa99e3eba4890d351b8639fb
AyaneSHOJI <shojiayane@gmail.com>
2022-06-03 11:27:48 +0200

    // p. 213 added Records class

---
+++ cs10net6chapter05/Records.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+// for avoir inti error
+namespace System.Runtime.CompilerServices
+{
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    internal class IsExternalInit { }
+}
+
+namespace Packt.Shared
+{
+    public class ImmutablePerson
+    {
+        public string? FirstName { get; init; }
+        public string? LastName { get; init; }
+    }
+}
